1023cd24054882a2723b3b7600053a4e9e9cf420
VictorPedraza <vapedraza1706@gmail.com>
2024-10-23 23:29:19 -0500

    chore: added api prefix path

--- src/_config/api.ts
+++ src/_config/api.ts
@@ -1,4 +1,5 @@
 export const IP = 'localhost';
-export const MAIN_API_HOST = `http://${IP}:8000`;
-//export const MAIN_API_HOST = `https://${IP}`;
+export const API_PREFIX_PATH = '/api';
+export const MAIN_API_HOST = `http://${IP}:8000${API_PREFIX_PATH}`;
+//export const MAIN_API_HOST = `https://${IP}${API_PREFIX_PATH}`;
 export const EMPLOYEES_EXCEL_TEMPLATE_URL = 'https://koalasoft.com.co/downloads/plantilla-empleados.xlsx';
